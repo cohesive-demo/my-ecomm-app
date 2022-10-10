8fcfb1a9821e7d40ad069524a0c8793a489d528e
shreyabhageria <shreyatest796@gmail.com>
2022-10-10 16:19:39 +0530

    added logs

--- orders/main.go
+++ orders/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"github.com/testOrgHimali/app/pkg/utils"
 	"net/http"
 )
 
 func hello(w http.ResponseWriter, _ *http.Request) {
-
+	log.Printf("Logging")
+log.Printf("Second line\n")
 	_, err := fmt.Fprintf(w, "Hello Orders %d \n", utils.Trigger())
 	if err != nil {
 		panic(err)
